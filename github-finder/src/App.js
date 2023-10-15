397bf361694b0509a442c6ee7fe6488ea30b6c08
Luis Zimermann <luisaslan@gmail.com>
2023-10-15 16:51:54 -0300

    API integration ready
    API integration ready

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
-  componentDidMount(){
-    
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({loading: true});
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({users: res.data, loading: false});
   }
 
   render() {
@@ -14,7 +25,10 @@ class App extends Component {
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github'/>
         <div className='container'>
-          <Users/>
+          <Users 
+            loading={this.state.loading}
+            users={this.state.users}
+          />
         </div>
       </div>
     );
--- github-finder/src/components/users/Users.js
+++ github-finder/src/components/users/Users.js
@@ -1,40 +1,29 @@
-import React, { Component } from 'react'
-import UserItem from './UserItem'
+import React from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types'
 
-class Users extends Component {
-    state = {
-        users: [
-            {
-                id: '1',
-                login: 'mojombo',
-                avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-                html_url: 'https://github.com/mojombo'
-            },
-            {
-                id: '2',
-                login: 'Luis',
-                avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-                html_url: 'https://github.com/mojombo'
-            },
-            {
-                id: '3',
-                login: 'Amanda',
-                avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-                html_url: 'https://github.com/mojombo'
-            }
-        ]
+
+const Users = ({users, loading}) => {
+    if (loading) {
+        return <Spinner/>
     }
-  render() {
-    return (
-      <div style={userStyle}>
-        {
-            this.state.users.map(user => (
-                <UserItem key={user.id} user={user}/>
-            ))
-        }
-      </div>
-    )
-  }
+    else {
+        return (
+          <div style={userStyle}>
+            {
+                users.map(user => (
+                    <UserItem key={user.id} user={user}/>
+                ))
+            }
+          </div>
+        )
+    }
+}
+
+Users.protoTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 const userStyle = {
