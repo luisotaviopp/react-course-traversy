273ee74ee1ff6607617b7ec6c8709f79b7ecad9a
Luis Zimermann <luisaslan@gmail.com>
2023-10-21 20:07:19 -0300

    Refactoring search from class to function component
    Refactoring search component from class to function component

--- github-finder/src/components/users/Search.js
+++ github-finder/src/components/users/Search.js
@@ -1,45 +1,33 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 
-export class Search extends Component {
-    state = {
-        text: ''
-    }
+const Search = ({searchUsers, showClear, clearUsers, setAlert}) => {
+    const [text, setText] = useState('');
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired,
-    };
-
-    onSubmit = e => {
+    const onSubmit = e => {
         e.preventDefault();
 
-        if (this.state.text === '') {
-            this.props.setAlert('Please enter something', 'light');
+        if (text === '') {
+            setAlert('Please enter something', 'light');
         } else {
-            this.props.searchUsers(this.state.text); // Passa o valor digitado como prop para o componente parent
-            this.setState({text: ''}) // limpa o texto digitado
+            searchUsers(text); // Passa o valor digitado como prop para o componente parent
+            setText('') // limpa o texto digitado
         }
     }
 
     // Capta o valor digitado no input
-    onChange = (e) => this.setState({[e.target.name]: e.target.value});
+    const onChange = (e) => setText(e.target.value);
 
-    render() {
-        const {showClear, clearUsers} = this.props;
-
-        return (
+    return (
         <div>
-            <form onSubmit={this.onSubmit} className='form'>
+            <form onSubmit={onSubmit} className='form'>
                 <input 
                     type='text' 
                     name='text' 
                     placeholder='Search Users...' 
-                    value={this.state.text}
-                    onChange={this.onChange}
+                    value={text}
+                    onChange={onChange}
                 />
 
                 <input
@@ -49,10 +37,22 @@ export class Search extends Component {
                 />
             </form>
             
-            {showClear &&  <button className='btn btn-light btn-block' onClick={clearUsers}>Clear</button>}
+            {showClear && (
+                <button className='btn btn-light btn-block' onClick={clearUsers}>
+                    Clear
+                </button>
+            )}
+
         </div>
-        )
-    }
+    )
 }
+
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+};
 
 export default Search
