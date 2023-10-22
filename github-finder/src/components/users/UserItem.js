3524cb83480dbadc978f4f79ad9a67cf14a5f695
Luis Zimermann <luisaslan@gmail.com>
2023-10-22 00:57:49 -0300

    ContextAPI finished.
    ContextAPI finished.

--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const UserItem = ({user: {login, avatar_url, html_url}}) => {
-
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
     <div className='card text-center'>
-      <img 
-        src={avatar_url} 
-        alt='' 
-        className='round-img' 
-        style={{width:'60px'}} 
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
       />
-
       <h3>{login}</h3>
+
       <div>
-        <Link to={`/user/${login}`} className='btn btn-dark btn-sn my-1'>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
           More
         </Link>
       </div>
@@ -24,7 +23,7 @@ const UserItem = ({user: {login, avatar_url, html_url}}) => {
 };
 
 UserItem.propTypes = {
-  user: PropTypes.object.isRequired,
-}
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
